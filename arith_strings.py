42c809f71d4400f30b1fc94ca32c39c9c31c4c2f
Severus <83870911+Ermek-dev@users.noreply.github.com>
2023-07-25 08:19:21 +0600

    Add files via upload
    Arithmetic strings
    3 lines are entered in random order. Write a program that finds out whether it is possible to construct an increasing arithmetic progression from the lengths of these strings.
    
    Input data format
    The input to the program is three lines, each on a separate line.
    
    Output format
    The program should output the string "YES" if it is possible to construct an arithmetic progression from the lengths of the entered words, "NO" otherwise.
    
    Test Data 🟢
    Sample Input 1:
    
    abc
    a
    abcde
    Sample Output 1:
    
    YES
    Sample Input 2:
    
    2434
    90099
    21
    Sample Output 2:
    
    NO

---
+++ arith_strings.py
@@ -0,0 +1,9 @@
+a,b,c = len(input()),len(input()),len(input())
+summa_numbers = a+b+c
+maximum_of_num = max(a,b,c)
+minimum_of_num = min(a,b,c)
+average_of_num = summa_numbers-maximum_of_num-minimum_of_num
+if maximum_of_num-average_of_num == average_of_num-minimum_of_num:
+    print("YES")
+else:
+    print("NO")    
